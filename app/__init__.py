dde6e987647e9a9a3bb550d7a0e2bee815ecf35f
Mia Kotalik <mia.kotalik@gmail.com>
2019-11-15 10:50:30 -0500

    secret key

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 # creates application object as instance of class Flask imported from the flask package
 from flask import Flask
+from config import Config
 
 
 # __name__ is python predefined variable which is set to the name of the module where its used
 app = Flask(__name__)
+app.config.from_object(Config)
 
 # imports the routes module
 from app import routes
