41239cced2e38959ae8528aadc1b39765bff4376
Mia Kotalik <mia.kotalik@gmail.com>
2019-11-15 10:18:25 -0500

    hello world complete

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 # creates application object as instance of class Flask imported from the flask package
-from flask import flask
+from flask import Flask
 
 
 # __name__ is python predefined variable which is set to the name of the module where its used
